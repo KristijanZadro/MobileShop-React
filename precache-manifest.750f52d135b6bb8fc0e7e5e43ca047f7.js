e9bb2d734531942ea1e319648952fd4c2b024fc6
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2021-04-27 14:56:41 +0200

    Updates

---
+++ precache-manifest.750f52d135b6bb8fc0e7e5e43ca047f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ea8bc34128c8e8f026521396dedd7a",
+    "url": "/MobileShop-React/index.html"
+  },
+  {
+    "revision": "2ed7a544df6988de9641",
+    "url": "/MobileShop-React/static/css/main.ac2c87e3.chunk.css"
+  },
+  {
+    "revision": "720b49622b0667d05743",
+    "url": "/MobileShop-React/static/js/2.5bbba38d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MobileShop-React/static/js/2.5bbba38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed7a544df6988de9641",
+    "url": "/MobileShop-React/static/js/main.492c1c35.chunk.js"
+  },
+  {
+    "revision": "a817b66f531cbc2a1c33",
+    "url": "/MobileShop-React/static/js/runtime-main.445ac9ca.js"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/MobileShop-React/static/media/loading-arrow.e9ea7394.gif"
+  }
+]);
